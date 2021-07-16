8ac9906f82b1fd7506904fbf57e3bc6a1c02aae4
Meenakshi McNamara <mcnama20@purdue.edu>
2021-07-16 14:55:56 -0400

    Upgraded a bunch and made some python

---
+++ Classifier_module.py
@@ -0,0 +1,72 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import argparse
+import os
+import numpy as np
+import math
+import sys
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F   # NOTE: I don't think this is used
+import torch.autograd as autograd
+import torch
+
+
+
+# In[ ]:
+
+class opt():   # Class used for optimizers in the future. Defines all variables and stuff needed.
+    save_weights = False
+    n_epochs = 30000   # an epoch is the number of times it works through the entire training set
+    batch_size = 5000   # the training set is broken up into batches, 
+                        # and the average loss is used from a given batch for back propagation
+    lr = 0.001   # learning rate (how much to change based on error)
+    b1 = 0.9   # Used for Adam. Exponential decay rate for the first moment
+    b2 = 0.999   # Used for Adam. Exponential decay rate for the second moment estimates (gradient squared)
+    input_size = 80
+    data_file = "/data"
+    config_dir = "."
+    save_location = config_dir + data_file
+    root_path = "/depot/darkmatter/data/jupyterhub/Physics_Undergrads/Steve/things"
+
+    file = root_path + "/all_1.root"
+
+    #n_cpu = 2    not used rn
+
+# os.makedirs(config_dir, exit_ok=True)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+# In[ ]:
+
+class Classifier(nn.Module):
+    """
+    classifier layers
+    """
+    def __init__(self):
+        super(Classifier, self).__init__()   # Just uses the module constructor with name Discriminator 
+
+        self.model = nn.Sequential(
+            nn.Linear(opt.input_size, 256),   # first layer
+            nn.LeakyReLU(0.2, inplace=True),   # apply leaky relu to layer
+            nn.Linear(256, 3),   # last layer
+            nn.LeakyReLU(0.2, inplace=True)   # apply leaky relu
+        )
+
+    def forward(self, input):
+        """
+        applies model to input and attempts to classify
+        """
+        output = self.model(input)   # Classifies the input (at location) as gg (0) qqbar (1) or other (2)
+        return output
+
